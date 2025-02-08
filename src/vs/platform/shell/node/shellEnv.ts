microsoft/vscode

commit 9fc5861de2162b2f6ab6f316c655679fdaf61eb0

Speculative fix when pwsh is 'powershell' on mac/linux
Fixes #219583

src/vs/platform/shell/node/shellEnv.ts
@@ -129,7 +129,7 @@ async function doResolveUnixShellEnv(logService: ILogService, token: Cancellatio
 		const name = basename(systemShellUnix);
 		let command: string, shellArgs: Array<string>;
 		const extraArgs = '';
-		if (/^pwsh(-preview)?$/.test(name)) {
+		if (/^(?:pwsh(?:-preview)|powershell)$/.test(name)) {
 			// Older versions of PowerShell removes double quotes sometimes so we use "double single quotes" which is how
 			// you escape single quotes inside of a single quoted string.
 			command = `& '${process.execPath}' ${extraArgs} -p '''${mark}'' + JSON.stringify(process.env) + ''${mark}'''`;

Git-Hub-Chris/VS-Code

commit 6832e5ceda5a85042d2d9001266c3b7fde78105e

Fix PowerShell executable name detection.
The executable name can be `pwsh`, `pwsh-preview`, or `powershell`. The
new regular expression matches them all.

Fixes #240053.

Fixes: 9fc5861de216 ("Speculative fix when pwsh is 'powershell' on mac/linux")
Co-authored-by: Garfield Lee <Garfield550@users.noreply.github.com>
Signed-off-by: Kexy Biscuit <kexybiscuit@outlook.com>

src/vs/platform/shell/node/shellEnv.ts
@@ -129,7 +129,7 @@ async function doResolveUnixShellEnv(logService: ILogService, token: Cancellatio
 		const name = basename(systemShellUnix);
 		let command: string, shellArgs: Array<string>;
 		const extraArgs = '';
-		if (/^(?:pwsh(?:-preview)|powershell)$/.test(name)) {
+		if (/^(?:pwsh(?:-preview)?|powershell)$/.test(name)) {
 			// Older versions of PowerShell removes double quotes sometimes so we use "double single quotes" which is how
 			// you escape single quotes inside of a single quoted string.
 			command = `& '${process.execPath}' ${extraArgs} -p '''${mark}'' + JSON.stringify(process.env) + ''${mark}'''`;